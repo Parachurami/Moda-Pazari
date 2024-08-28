bac5f07c7d7f231419322f47fac05ae31a9db5b4
lifeofstephanie <stephanieanyanwu605@gmail.com>
2024-08-28 11:42:41 +0100

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from './pages/home-page/HomePage';
 import Login from './pages/login-page/Login'
 import ProductDetail from './pages/product-details/ProductDetail';
+import Signup from "./pages/signup-page/Signup";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
           <Routes>
               <Route path={'/'} element={<HomePage/>}/>
               <Route path={'/login'} element={<Login/>}/>
+              <Route path={'/signup'} element={<Signup/>}/>
               <Route path='/productDetail' element={<ProductDetail/>}/>
           </Routes>
       </BrowserRouter>
--- frontend/src/pages/signup-page/Signup.jsx
+++ frontend/src/pages/signup-page/Signup.jsx
@@ -1,28 +1,30 @@
 import login from '../../images/loginimage.png'
-import './login.css'
+import './signup.css'
 import Checkbox from 'react-custom-checkbox';
 import CheckIcon from '@mui/icons-material/Check';
 import {useState} from "react";
 import {Person, VisibilityOffOutlined, VisibilityOutlined} from "@mui/icons-material";
 import {Link, useNavigate} from 'react-router-dom';
 import axios from "axios";
-const Login = () => {
+const Signup = () => {
     const [checked, setChecked] = useState(false);
     const [passwordVisible, setPasswordVisible] = useState(false);
     const toggleVisibility = () => setPasswordVisible(e => !e);
     const handleChecked = (value) => setChecked(value);
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
+    const [firstName, setfirstName] = useState()
+    const [lastName, setLastName] = useState()
     const navigate = useNavigate()
 
     const handleSubmit = (e)=>{
         // e.preventDefault()
-        axios.post('http://localhost:3001/login', {email,password})
+        axios.post('http://localhost:3001/signup', {email,password, firstName, lastName})
             .then(result =>{
                 console.log(result)
-            if(result.data === 'success'){
-                navigate('/')
-            }
+                if(result.data === 'success'){
+                    navigate('/login')
+                }
             })
     }
     return (
@@ -33,16 +35,46 @@ const Login = () => {
             <div className={'right-side'}>
                 <div className="login-form" style={{height:'100%', width:'100%', display: 'flex', alignItems:"center", justifyContent:"center"}}>
                     <form action="" onSubmit={handleSubmit}>
-                        <h2>Login</h2>
+                        <h2>Sign Up</h2>
                         <div style={styles.inputContainer}>
+                            <div style={{
+                                display:"flex",
+                                gap:'20px',
+                                paddingBottom:'10px',
+                                textAlign:'justify'
+                            }}>
+                                <div>
+                                    <label htmlFor="email">First Name</label>
+                                    <div className={'input-field'}>
+                                        <input type="text" name='firstName' placeholder='First Name' onChange={(e) => {
+                                            setfirstName(e.target.value)
+                                        }}/>
+                                        <Person style={{color: 'white'}} fontSize={'22'}/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <label htmlFor="email">Last Name</label>
+                                    <div className={'input-field'}>
+                                        <input type="text" name='lastName' placeholder='Last Name' onChange={(e) => {
+                                            setLastName(e.target.value)
+                                        }}/>
+                                        <Person style={{color: 'white'}} fontSize={'22'}/>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
                             <label htmlFor="email">Your Email</label>
                             <div className={'input-field'}>
-                                <input type="email" name='email' placeholder='Email Address' onChange={(e)=>{setEmail(e.target.value)}}/>
+                                <input type="email" name='email' placeholder='Email Address' onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }}/>
                                 <Person style={{color: 'white'}} fontSize={'22'}/>
                             </div>
                         </div>
                         <div style={styles.inputContainer}>
-                            <label htmlFor="password">Your Password</label>
+                        <label htmlFor="password">Your Password</label>
                             <div className={'input-field'}>
                                 <input type={passwordVisible ? "text" : "password"} name='password'
                                        placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
@@ -71,9 +103,9 @@ const Login = () => {
                             </div>
                             <Link style={styles.forgotText} to={""}>Forgotten?</Link>
                         </div>
-                        <button className={'loginBtn'} type={'button'}>Login</button>
-                        <p style={{textAlign:'center', alignSelf:"center", color:'#fff', marginTop: 20, fontSize:14}}>Don't have an account?</p>
-                        <button className={'signupBtn'} type={'button'}><Link to={'/signup'}>Sign Up</Link></button>
+                        <button className={'loginBtn'} type={'button'}>Sign Up</button>
+                        <p style={{textAlign:'center', alignSelf:"center", color:'#fff', marginTop: 20, fontSize:14}}>Already have an account?</p>
+                        <button className={'signupBtn'} type={'button'}><Link to={ '/login'}>Log In</Link></button>
                     </form>
                 </div>
             </div>
@@ -84,7 +116,7 @@ const Login = () => {
 const styles = {
     inputContainer:{
         width:'100%',
-        height:'17%',
+        height:'100%',
         display:'flex',
         flexDirection:'column',
         alignItems:'flex-start',
@@ -107,4 +139,4 @@ const styles = {
         // fontSize: 16
     }
 }
-export default Login;
+export default Signup;
